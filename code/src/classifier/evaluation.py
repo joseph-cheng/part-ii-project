c5c325a33a3ddacc1af40014325868d2ea1fcea1
Joseph Cheng <joe.cheng2001@gmail.com>
2022-04-03 19:20:46 +0100

    almost finished evaluation chapter

--- code/src/classifier/evaluation.py
+++ code/src/classifier/evaluation.py
@@ -150,11 +150,15 @@ def evaluate_metrics(data_dir, metrics, transforms=[]):
 if __name__ == "__main__":
     # need to cast to list because we consume the generator in making metric_results
     #transform_combinations = list(itertools.chain.from_iterable(itertools.combinations(TRANSFORMS, i) for i in range(0, len(TRANSFORMS)+1)))
-    transform_combinations= [(unique_reverb.UniqueReverb(["../../res/irs/studio.wav", "../../res/irs/church.wav", "../../res/irs/dales.wav", "../../res/irs/sportscentre.wav"]),)]
+    transform_combinations = [
+            (reverb.Reverb("../../res/irs/church.wav"),),
+            (reverb.Reverb("../../res/irs/sportscentre.wav"),),
+            ]
     metric_results = {transform_combination: {} for transform_combination in transform_combinations}
     for transform_combination in transform_combinations:
 
         metric_combinations = itertools.chain.from_iterable(itertools.combinations(metric_calculator.METRICS, i) for i in range(1, len(metric_calculator.METRICS)+1))
+        metric_combinations = [(metric_calculator.METRICS[1],)]
         for metric_combination in metric_combinations:
             metric_results[transform_combination][metric_combination] = evaluate_metrics(data_dir, metric_combination, transforms=transform_combination)
 
--- code/src/classifier/metrics/dynamics.py
+++ code/src/classifier/metrics/dynamics.py
@@ -99,6 +99,15 @@ class DynamicsCalculator(metric.MetricCalculator):
         power_array1 = power_array1 / highest_value
         power_array2 = power_array2 / highest_value
 
+        # PLOTTING CODE
+        plt.rcParams.update({'font.size': 30})
+        plt.plot(power_array1, label=audio1.name)
+        plt.plot(power_array2, label=audio2.name)
+        plt.xticks([])
+        plt.yticks([])
+        plt.legend()
+        plt.show()
+
         # now we find the sum of the squared errors
 
         squared_differences_sum = np.sum((power_array1 - power_array2)**2)
